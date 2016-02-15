5906da149a154c9132cd0343620b97e1e8cd8428
Shawn Bird <shawn.cameron.bird@gmail.com>
2016-02-15 16:31:13 +0100

    Matcher: Rearrange and memoize
    PathMatch now contains most of the logic so that it's convenient to memoize the
    main score recursive computation on a per-path basis. Also:
    
    - Add missing 'optparse' require to fuzmatch
    - Remove checks for dotfiles
    - Avoid recursive call when there aren't enough path chars to satisfy query
    - Tweak error messages

--- matcher.rb
+++ matcher.rb
@@ -6,7 +6,7 @@ class Matcher
   UPPER = ('A'..'Z')
 
   class Query
-    attr_reader :query
+    attr_reader :query, :query_len
 
     def initialize(query)
       @query = query
@@ -14,41 +14,55 @@ class Matcher
     end
 
     def score_path(path)
-      p = Path.new(path, @query_len)
-      p.score = case
-      when @query_len > p.path_len
-        0.0
-      when @query_len == p.path_len
-        @query.casecmp(p.path) == 0 ? 1.0 : 0.0
-      else
-        self.compute_subscore(p)
-      end
-      p
+      PathMatch.new(path, self)
     end
 
-    def compute_subscore(p, query_beg = 0, path_beg = 0, last_path_pos = -1)
+  end
+
+  class PathMatch
+    attr_reader :path, :path_len, :max_score_per_char
+    attr_accessor :score, :dot_file
+
+    def initialize(path, q)
+      @path = path
+      @path_len = path.length()
+      @max_score_per_char = (1.0 / @path_len + 1.0 / q.query_len) / 2.0
+      @dot_file = false
+      @subscores = Hash.new do |h, args|
+        q = args.shift
+        h[args] = self.send(:compute_subscore, q, *args)
+      end
+      @score =
+        case
+        when q.query_len < @path_len
+          self.compute_subscore(q)
+        when q.query_len > @path_len
+          0.0
+        else
+          @path.casecmp(q.query) == 0 ? 1.0 : 0.0
+        end
+    end
+
+    def compute_subscore(q, query_beg = 0, path_beg = 0, last_path_pos = -1)
       best_score = 0.0
       score = 0.0
-      pc_prev = path_beg > 0 ? p.path[path_beg - 1] : nil
-      for query_pos in query_beg...@query_len
+      pc_prev = path_beg > 0 ? @path[path_beg - 1] : nil
+      for query_pos in query_beg...q.query_len
         qc_match = false
-        qc = @query[query_pos]
-        qc_is_dot = qc == '.'
-        for path_pos in path_beg...p.path_len
-          pc = p.path[path_pos]
-          p.dot_file = true if qc_is_dot && (
-            pc_prev.nil? || pc_prev == File::SEPARATOR)
+        qc = q.query[query_pos]
+        for path_pos in path_beg...@path_len
+          pc = @path[path_pos]
           if qc.casecmp(pc) == 0
             qc_match = true
-            pc_score = p.max_score_per_char
+            pc_score = @max_score_per_char
             distance = path_pos - last_path_pos
-            if distance > 1 && !(qc == File::SEPARATOR || qc_is_dot)
+            if distance > 1 && !(qc == File::SEPARATOR || qc == '.')
               pc_score *= self.compute_factor(pc, pc_prev, distance)
             end
             next_path_pos = path_pos + 1
-            if next_path_pos < p.path_len
-              score_alt = score + self.compute_subscore(
-                p, query_pos, next_path_pos, last_path_pos)
+            if @path_len - next_path_pos > q.query_len - query_pos
+              score_alt = score +
+                @subscores[[q, query_pos, next_path_pos, last_path_pos]]
               best_score = score_alt if score_alt > best_score
             end
             score += pc_score
@@ -80,19 +94,6 @@ class Matcher
         # as distance from last matched char increases
         (1.0 / distance) * 0.75;
       end
-    end
-  end
-
-  class Path
-    attr_reader :path, :path_len, :max_score_per_char
-    attr_accessor :score, :dot_file
-
-    def initialize(path, query_len)
-      @path = path
-      @path_len = path.length()
-      @score = 0.0
-      @max_score_per_char = (1.0 / @path_len + 1.0 / query_len) / 2.0
-      @dot_file = false
     end
   end
 end
